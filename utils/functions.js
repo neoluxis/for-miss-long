d1de092cf6ac60ec07ca2429b0bd64c8d3f0d1c5
neoluxis <neolux.lee@neolux.ga>
2022-11-10 22:07:12 +0800

    update

--- utils/functions.js
+++ utils/functions.js
@@ -51,6 +51,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = " <span class=\"digit\">" + days + "</span>  + days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds";
+	var result = " <span class=\"digit\">" + days + "</span>days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds";
 	$("#clock").html(result);
 }
